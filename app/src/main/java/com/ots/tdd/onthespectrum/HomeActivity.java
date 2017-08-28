63efeadc42196ac8418dd265de5a4daf602dc18c
ETVargas <evargas30@gatech.edu>
2017-08-28 03:06:21 -0400

    Created empty activities for five screens: Home, Call, Emergency List, and Settings.
    Added basic navigation from the Home screen to all others.

---
+++ app/src/main/java/com/ots/tdd/onthespectrum/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.ots.tdd.onthespectrum;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.app.ActionBar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        
+        // Show the action bar and return arrow at the top of the screen
+        // TODO: Fix this.
+        // Potential fix: https://stackoverflow.com/questions/24596494/android-app-crashes-on-startup-in-emulator
+        /*ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);*/
+    }
+
+    public void moveToCallScreen(View v) {
+        Intent intentCall = new Intent(this, CallActivity.class);
+        startActivity(intentCall);
+    }
+
+    public void moveToProfileScreen(View v) {
+        Intent intentProfile = new Intent(this, ProfileActivity.class);
+        startActivity(intentProfile);
+    }
+
+    public void moveToSettingsScreen(View v) {
+        Intent intentSettings = new Intent(this, SettingsActivity.class);
+        startActivity(intentSettings);
+    }
+
+    public void moveToEmergencyListScreen(View v) {
+        Intent intentEmergencyList = new Intent(this, EmergencyListActivity.class);
+        startActivity(intentEmergencyList);
+    }
+}
---
+++ app/src/main/java/com/ots/tdd/onthespectrum/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.ots.tdd.onthespectrum;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Show the action bar and return arrow at the top of the screen
+        /*ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);*/
+    }
+}
