261e5b94ba638869c7a743191295d6806f5ba22d
Frances Tsenn <ftsenn@gmail.com>
2017-09-07 08:10:06 -0400

    Can add information to the profile list

--- app/src/main/java/com/ots/tdd/onthespectrum/ProfileActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/ProfileActivity.java
@@ -1,9 +1,8 @@
 package com.ots.tdd.onthespectrum;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -16,27 +15,25 @@ import java.util.Arrays;
 
 public class ProfileActivity extends AppCompatActivity {
     ArrayAdapter<ProfileElement> adapter;
-    ArrayAdapter<String> adapter2;
-    EditText editText;
+    EditText editInfoType;
+    EditText editUserInfo;
+    TextView alertTextView;
+
     ArrayList<ProfileElement> itemList;
     ProfileElement[] test = {
-            new ProfileElement("Apple", "1"),
-            new ProfileElement("Banana", "2"),
-            new ProfileElement("Clementine", "3"),
+            new ProfileElement("Name", "Testing Name"),
+            new ProfileElement("Birth Date", "September 7, 2017"),
+            new ProfileElement("Gender", "Female"),
     };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        String[] items={"Apple","Banana","Clementine"};
-
-        //itemList=new ArrayList<String>(Arrays.asList(items));
-        //test = new ArrayList<String>(Arrays.asList(testArr));
         itemList = new ArrayList<ProfileElement>(Arrays.asList(test));
-        adapter=new ArrayAdapter<ProfileElement>(this, 0, itemList) {
+        adapter=new ArrayAdapter<ProfileElement>(this, R.layout.profile_item, itemList) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                ProfileElement current = test[position];
+                ProfileElement current = itemList.get(position);
 
                 // Inflate only once
                 if(convertView == null) {
@@ -55,16 +52,29 @@ public class ProfileActivity extends AppCompatActivity {
         };
         ListView listV=(ListView)findViewById(R.id.list);
         listV.setAdapter(adapter);
-        editText=(EditText)findViewById(R.id.txtInput);
+        editInfoType=(EditText)findViewById(R.id.infoType);
+        editUserInfo = (EditText)findViewById(R.id.userInfo);
+        alertTextView = (TextView)findViewById(R.id.alert);
         Button btAdd=(Button)findViewById(R.id.btAdd);
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newItem=editText.getText().toString();
-                // add new item to arraylist
-                //itemList.add(new ProfileElement(newItem, "testblah"));
-                // notify listview of data changed
-                adapter.notifyDataSetChanged();
+                String newType = editInfoType.getText().toString();
+                String newInfo = editUserInfo.getText().toString();
+
+                if (!newType.isEmpty() && !newInfo.isEmpty()) {
+                    alertTextView.setTextColor(Color.argb(0, 255, 64, 129));
+                    // add new item to arraylist
+                    itemList.add(new ProfileElement(newType, newInfo));
+                    // notify listview of data changed
+                    adapter.notifyDataSetChanged();
+
+                    // resets the text boxes
+                    editInfoType.setText("");
+                    editUserInfo.setText("");
+                } else {
+                    alertTextView.setTextColor(Color.argb(255, 255, 64, 129));
+                }
             }
 
         });
