86b30d2b5556bbd2050d3d7e8f233e8451ef4184
Frances Tsenn <ftsenn@gmail.com>
2017-09-07 00:06:23 -0400

    Began template for re-formating the profile for creating additional fields

---
+++ app/src/main/java/com/ots/tdd/onthespectrum/ProfileElement.java
@@ -0,0 +1,15 @@
+package com.ots.tdd.onthespectrum;
+
+/**
+ * Created by francestsenn on 9/6/17.
+ */
+
+public class ProfileElement {
+    String infoType;
+    String userInfo;
+
+    public ProfileElement(String infoType, String userInfo) {
+        this.infoType = infoType;
+        this.userInfo = userInfo;
+    }
+}
