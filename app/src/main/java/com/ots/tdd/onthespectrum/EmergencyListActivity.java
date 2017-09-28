1b3c3b8c1eae2c575ead4437a7efd8600e822221
viyer014 <viyer014@gatech.edu>
2017-09-28 02:50:53 -0400

    SPEC-13 View Existing List of Emergencies

--- app/src/main/java/com/ots/tdd/onthespectrum/EmergencyListActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/EmergencyListActivity.java
@@ -1,16 +1,11 @@
 package com.ots.tdd.onthespectrum;
 
-import android.app.ActionBar;
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class EmergencyListActivity extends AppCompatActivity {
 
@@ -18,15 +13,15 @@ public class EmergencyListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_list);
+        ImageButton btn = (ImageButton) findViewById(R.id.imageButton10);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
-
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(EmergencyListActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
+        btn.setOnClickListener(new View.OnClickListener(){
+            // When the button is pressed/clicked, it will run the code below
+            @Override
+            public void onClick(View view){
+                // Intent is what you use to start another activity
+                Intent intent = new Intent(EmergencyListActivity.this, SelectedEmergencyActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -35,55 +30,6 @@ public class EmergencyListActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);*/
     }
 
-    public class ImageAdapter extends BaseAdapter {
-        private Context mContext;
 
-        public ImageAdapter(Context c) {
-            mContext = c;
-        }
-
-        public int getCount() {
-            return mThumbIds.length;
-        }
-
-        public Object getItem(int position) {
-            return null;
-        }
-
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        // create a new ImageView for each item referenced by the Adapter
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-            if (convertView == null) {
-                // if it's not recycled, initialize some attributes
-                imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(8, 8, 8, 8);
-            } else {
-                imageView = (ImageView) convertView;
-            }
-
-            imageView.setImageResource(mThumbIds[position]);
-            return imageView;
-        }
-
-        // references to our images
-        private Integer[] mThumbIds = {
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7
-        };
-    }
 }
+
