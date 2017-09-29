9aeff34492632b03f4025125bcecb0aaa4127b66
ETVargas <evargas30@gatech.edu>
2017-09-29 19:05:34 -0400

    Number being called can be specified in the Call screen.

--- app/src/main/java/com/ots/tdd/onthespectrum/CallActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/CallActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CallActivity extends AppCompatActivity {
 
@@ -21,8 +23,34 @@ public class CallActivity extends AppCompatActivity {
     }
 
     public void initiateCall(View v) {
-        Intent intentOngoingCall = new Intent(this, OngoingCallActivity.class);
-        intentOngoingCall.putExtra("TELEPHONE_NUMBER", telNum);
-        startActivity(intentOngoingCall);
+
+        EditText txtPhn = (EditText)findViewById(R.id.phoneNumberEditText);
+        telNum = txtPhn.getText().toString();
+        if (telNum.length() == 10) {
+            boolean valid = true;
+            for (int i = 0; i < 10; i++) {
+                if(!Character.isDigit(telNum.charAt(i))) {
+                    valid = false;
+                }
+            }
+            if (valid) {
+                Intent intentOngoingCall = new Intent(this, OngoingCallActivity.class);
+                intentOngoingCall.putExtra("TELEPHONE_NUMBER", telNum);
+                startActivity(intentOngoingCall);
+            } else {
+                String errorMessage = "Invalid phone number (not digit): " + telNum;
+                displayExceptionMessage(errorMessage);
+            }
+        } else {
+            String errorMessage = "Invalid phone number (too short): " + telNum + ": " + telNum.length();
+            displayExceptionMessage(errorMessage);
+        }
+
+
+    }
+
+    public void displayExceptionMessage(String msg)
+    {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
