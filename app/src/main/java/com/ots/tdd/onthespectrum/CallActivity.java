b5b73bbec653686421400764a1fe44ca15b9fc13
ETVargas <evargas30@gatech.edu>
2017-09-29 16:31:59 -0400

    Calls can be made through the native Phone application.

--- app/src/main/java/com/ots/tdd/onthespectrum/CallActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/CallActivity.java
@@ -1,10 +1,14 @@
 package com.ots.tdd.onthespectrum;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CallActivity extends AppCompatActivity {
+
+    String telNum = "4706293412";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,11 @@ public class CallActivity extends AppCompatActivity {
         // Show the action bar and return arrow at the top of the screen
         /*ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);*/
+    }
+
+    public void initiateCall(View v) {
+        Intent intentOngoingCall = new Intent(this, OngoingCallActivity.class);
+        intentOngoingCall.putExtra("TELEPHONE_NUMBER", telNum);
+        startActivity(intentOngoingCall);
     }
 }
