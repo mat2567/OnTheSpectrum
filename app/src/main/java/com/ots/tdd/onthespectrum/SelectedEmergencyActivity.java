f69d3eef1826e2eb196910f209619e36754ce0bb
Frances Tsenn <ftsenn@gmail.com>
2017-09-29 23:13:34 -0400

    Calls a phone number and sends information to speak

--- app/src/main/java/com/ots/tdd/onthespectrum/SelectedEmergencyActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/SelectedEmergencyActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,6 +19,8 @@ public class SelectedEmergencyActivity extends AppCompatActivity {
 
     ScrollView infoScrollView;
     ArrayList<ProfileElement> profArray = new ArrayList<>();
+    String telNum = "8322645195";
+    String toSpeak = "Hello.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,7 @@ public class SelectedEmergencyActivity extends AppCompatActivity {
         }
 
         String infoText = "What will be said on your call:\n\nHello. " + appInfo + scenarioInfo + " " + profInfo;
+        toSpeak = "Hello. " + appInfo + scenarioInfo + " " + profInfo;
 
         infoScrollView = (ScrollView) findViewById(R.id.infoScrollView);
         TextView infoTextView = new TextView(this);
@@ -58,5 +64,38 @@ public class SelectedEmergencyActivity extends AppCompatActivity {
                 profArray.add(new ProfileElement(fields[i], info, i));
             }
         }
+    }
+
+    public void initiateCall(View v) {
+
+        //EditText txtPhn = (EditText)findViewById(R.id.phoneNumberEditText);
+        //telNum = txtPhn.getText().toString();
+        if (telNum.length() == 10) {
+            boolean valid = true;
+            for (int i = 0; i < 10; i++) {
+                if(!Character.isDigit(telNum.charAt(i))) {
+                    valid = false;
+                }
+            }
+            if (valid) {
+                Intent intentOngoingCall = new Intent(this, OngoingCallActivity.class);
+                intentOngoingCall.putExtra("TELEPHONE_NUMBER", telNum);
+                intentOngoingCall.putExtra("TO_SPEAK", toSpeak);
+                startActivity(intentOngoingCall);
+            } else {
+                String errorMessage = "Invalid phone number (not digit): " + telNum;
+                displayExceptionMessage(errorMessage);
+            }
+        } else {
+            String errorMessage = "Invalid phone number (too short): " + telNum + ": " + telNum.length();
+            displayExceptionMessage(errorMessage);
+        }
+
+
+    }
+
+    public void displayExceptionMessage(String msg)
+    {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
