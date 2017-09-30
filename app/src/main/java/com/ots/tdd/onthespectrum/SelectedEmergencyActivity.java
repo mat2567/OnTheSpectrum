d0706179f9fca371b95d3e91cf43a97c6d9acb14
ETVargas <evargas30@gatech.edu>
2017-09-29 20:33:13 -0400

    Moved calling functionality from Call screen to SelectedEmergency screen.

--- app/src/main/java/com/ots/tdd/onthespectrum/SelectedEmergencyActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/SelectedEmergencyActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SelectedEmergencyActivity extends AppCompatActivity {
+
+    String telNum = "4706293412";
 
     ScrollView infoScrollView;
 
@@ -24,5 +29,37 @@ public class SelectedEmergencyActivity extends AppCompatActivity {
         TextView infoTextView = new TextView(this);
         infoTextView.setText(text);
         infoScrollView.addView(infoTextView);
+    }
+
+    public void initiateCall(View v) {
+
+        EditText txtPhn = (EditText)findViewById(R.id.phoneNumberEditText);
+        telNum = txtPhn.getText().toString();
+        if (telNum.length() == 10) {
+            boolean valid = true;
+            for (int i = 0; i < 10; i++) {
+                if(!Character.isDigit(telNum.charAt(i))) {
+                    valid = false;
+                }
+            }
+            if (valid) {
+                Intent intentOngoingCall = new Intent(this, OngoingCallActivity.class);
+                intentOngoingCall.putExtra("TELEPHONE_NUMBER", telNum);
+                startActivity(intentOngoingCall);
+            } else {
+                String errorMessage = "Invalid phone number (not digit): " + telNum;
+                displayExceptionMessage(errorMessage);
+            }
+        } else {
+            String errorMessage = "Invalid phone number (too short): " + telNum + ": " + telNum.length();
+            displayExceptionMessage(errorMessage);
+        }
+
+
+    }
+
+    public void displayExceptionMessage(String msg)
+    {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
