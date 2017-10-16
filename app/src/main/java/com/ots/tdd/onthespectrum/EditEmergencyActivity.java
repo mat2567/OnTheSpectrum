ad24ba313310f8612385bb5e5073c796d848b089
ETVargas <evargas30@gatech.edu>
2017-10-16 16:45:41 -0400

    Attempting to add image editting for emergency scenarios.

--- app/src/main/java/com/ots/tdd/onthespectrum/EditEmergencyActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/EditEmergencyActivity.java
@@ -1,19 +1,28 @@
 package com.ots.tdd.onthespectrum;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class EditEmergencyActivity extends AppCompatActivity {
 
     public EmergencyElement emergency;
     public EditText emergencyTitle;
     public ImageButton emergencyImage;
+
+    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +57,48 @@ public class EditEmergencyActivity extends AppCompatActivity {
 
     public void cancelChanges(View v) {
         finish();
+    }
+
+    public void changeImage(View v) {
+        toastMessage("Changing Image");
+
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        toastMessage("Loading in Image");
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            emergencyImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
+
+    }
+
+    public void toastMessage(String msg) {
+        Context context = getApplicationContext();
+        CharSequence text = msg;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast.makeText(context, text, duration).show();
     }
 }
