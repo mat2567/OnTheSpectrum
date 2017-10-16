df0882d09146602b83dbfc765a574f56ea428b2b
ETVargas <evargas30@gatech.edu>
2017-10-16 17:45:11 -0400

    Added editing functionality to emergency scenario images. Allows user to load from saved images. Currently stretches image to fit.

--- app/src/main/java/com/ots/tdd/onthespectrum/EditEmergencyActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/EditEmergencyActivity.java
@@ -3,8 +3,10 @@ package com.ots.tdd.onthespectrum;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +26,8 @@ public class EditEmergencyActivity extends AppCompatActivity {
 
     private static int RESULT_LOAD_IMAGE = 1;
 
+    BitmapFactory.Options options;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +39,6 @@ public class EditEmergencyActivity extends AppCompatActivity {
 
         emergencyTitle.setText(emergency.getTitle());
         emergencyImage.setBackground( new BitmapDrawable(getResources(), emergency.getImage()) );
-        emergencyImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Change image
-            }
-        });
         emergencyImage.setLayoutParams(new LinearLayout.LayoutParams(350, 350)); //currently hardcoded, change later
 
 
@@ -48,7 +46,8 @@ public class EditEmergencyActivity extends AppCompatActivity {
 
     public void saveChanges(View v) {
         emergency.setTitle(emergencyTitle.getText().toString());
-        emergency.setImage(((BitmapDrawable)emergencyImage.getBackground()).getBitmap());
+        //emergency.setImage(((BitmapDrawable)emergencyImage.getBackground()).getBitmap());
+        emergency.setImage(((BitmapDrawable)emergencyImage.getDrawable()).getBitmap());
 
         // notify gridview of data changed
         ListOfEmergenciesActivity.adapter.notifyDataSetChanged();
@@ -73,22 +72,19 @@ public class EditEmergencyActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        toastMessage("Loading in Image");
 
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            emergencyImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-
+            Uri photoUri = data.getData();
+            // Do something with the photo based on Uri
+            //https://guides.codepath.com/android/Accessing-the-Camera-and-Stored-Media#accessing-stored-media
+            try {
+                Bitmap selectedImg = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+                // Load the selected image into a preview
+                emergencyImage.setImageBitmap(selectedImg);
+            } catch (Exception e) {
+                toastMessage("Choose a different image");
+            }
+            // scale this later
         }
 
 
