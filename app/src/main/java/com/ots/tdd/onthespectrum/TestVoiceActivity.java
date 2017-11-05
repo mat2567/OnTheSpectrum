8e8037f9f74872b07db0ce71208925902d00bfd9
arsh786 <amzehra@gmail.com>
2017-11-05 09:35:15 -0500

    Attempt at GET

--- app/src/main/java/com/ots/tdd/onthespectrum/TestVoiceActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/TestVoiceActivity.java
@@ -1,9 +1,17 @@
 package com.ots.tdd.onthespectrum;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.Locale;
 
 
@@ -17,6 +25,23 @@ public class TestVoiceActivity extends AppCompatActivity implements TextToSpeech
 
         ttobj=new TextToSpeech(this, this);
 
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://ots-v1.herokuapp.com/text-to-speech/";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("Volley", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Volley", "nope");
+            }
+        });
+        queue.add(stringRequest);
     }
 
     @Override
