a393fb3b5d79f0f514cf644c4af325e82922d986
arsh786 <amzehra@gmail.com>
2017-12-04 17:17:31 -0500

    got Google Maps link working on send text #done

--- app/src/main/java/com/ots/tdd/onthespectrum/TestVoiceActivity.java
+++ app/src/main/java/com/ots/tdd/onthespectrum/TestVoiceActivity.java
@@ -5,19 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.media.AudioManager;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,7 +22,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
 
 
 // URL To Initiate Call:    https://ots-plivo-connection.herokuapp.com/initiate_call/
@@ -108,13 +104,32 @@ public class TestVoiceActivity extends AppCompatActivity{ //implements EventList
     public void sendText(String careTakerNum) {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.SEND_SMS},1);
-
+        ArrayList<String> messages = new ArrayList<>();
         String message =
                 "Hello Caretaker of "+sharedPref.getString("Name", "OnTheSpectrum user")+",\n" +
-                "I am in a "+ sharedPref.getString("ScenarioNames", "emergency") +
-                " situation right now at "+ location+". " + //get location
-                "I have contacted emergency services";
-        //, and they should be reaching out to you shortly.
+                "I am in a "+ sharedPref.getString("ScenarioNames", "emergency") + ".";
+        messages.add(message);
+        String ns = "";
+        String ew = "";
+        int sep = 0;
+
+        if (location.indexOf('N') != -1) {
+            ns = location.substring(0, location.indexOf('N') -1);
+            sep = location.indexOf('N') + 2;
+        } else if (location.indexOf('S') != -1) {
+            ns = "-" + location.substring(0, location.indexOf('S') -1);
+            sep = location.indexOf('S') + 2;
+        }
+
+        if (location.indexOf('E') != -1) {
+            ew = location.substring(sep, location.indexOf('E'));
+        } else if (location.indexOf('W') != -1) {
+            ew = "-" + location.substring(sep, location.indexOf('W'));
+        }
+        String googleURL = "http://www.google.com/maps/place/" +ns + "," + ew;
+
+        String more  = "I have contacted emergency services. Here is my location: " + googleURL;
+        messages.add(more);
         int permissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.SEND_SMS);
         while(permissionCheck == PackageManager.PERMISSION_DENIED) {
@@ -124,8 +139,8 @@ public class TestVoiceActivity extends AppCompatActivity{ //implements EventList
         if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
             SmsManager smsManager = SmsManager.getDefault();
             if (careTakerNum != null) {
-                smsManager.sendTextMessage(careTakerNum,
-                        null, message, null, null);
+                smsManager.sendMultipartTextMessage(careTakerNum,
+                        null, messages, null, null);
             }
         }
     }
